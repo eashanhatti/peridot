2059998ee675a2ffdf71b4746556ceb8fa2e215c
Eashan Hatti <eashanhatti@gmail.com>
2022-02-13 22:28:51 -0500

    Allow for overloading

--- src/Elaboration/Effect.hs
+++ src/Elaboration/Effect.hs
@@ -3,9 +3,11 @@ module Elaboration.Effect where
 import Control.Effect.State(State, get, put)
 import Control.Effect.Reader(Reader, ask, local)
 import Control.Effect.Throw(Throw)
+import Control.Effect.NonDet(NonDet)
 import Control.Algebra(Has)
 import Data.Set(Set, singleton)
-import Data.Map(Map, (!), insert, union, fromList)
+import Data.Set qualified as Set
+import Data.Map(Map, (!), insert, union, fromList, lookup)
 import Syntax.Variable hiding (unId)
 import Syntax.Core qualified as C
 import Syntax.Semantic qualified as N
@@ -21,7 +23,10 @@ import Data.Type.Equality
 import Data.Hashable
 import GHC.Generics hiding (Constructor)
 import Normalization
+import Unification
 import Numeric.Natural
+import Data.Foldable(toList)
+import Prelude hiding(lookup)
 
 data QueryState = QueryState
   { unMemoTable :: DHashMap Key Identity }
@@ -33,18 +38,15 @@ data Error
 type Query sig m = Has (State QueryState) sig m
 
 data Key a where
-  ElabDecl :: Predeclaration -> Key C.Declaration
-  ElabTerm :: TermAst -> Key C.Term
+  CheckDecl :: Predeclaration -> Key C.Declaration
 
 instance GEq Key where
-  geq (ElabDecl _) (ElabDecl _) = Just Refl
-  geq (ElabTerm _) (ElabTerm _) = Just Refl
+  geq (CheckDecl _) (CheckDecl _) = Just Refl
   geq _ _ = Nothing
 
 instance Hashable (Some Key) where
-  hashWithSalt salt (Some (ElabDecl (PDDecl (DeclAst _ did)))) = salt `hashWithSalt` did
-  hashWithSalt salt (Some (ElabDecl (PDDecl (ConstrAst _ did _)))) = salt `hashWithSalt` did
-  hashWithSalt salt (Some (ElabTerm (TermAst _ did))) = salt `hashWithSalt` did
+  hashWithSalt salt (Some (CheckDecl (PDDecl (DeclAst _ did)))) = salt `hashWithSalt` did
+  hashWithSalt salt (Some (CheckDecl (PDDecl (ConstrAst _ did _)))) = salt `hashWithSalt` did
 
 memo :: Query sig m => Key a -> m a -> m a
 memo key act = do
@@ -57,21 +59,34 @@ memo key act = do
       pure result
 
 data Binding = BLocal Index N.Term | BGlobal Id
+  deriving (Eq)
+
+instance Ord Binding where
+  compare (BLocal ix1 _) (BLocal ix2 _) = compare ix1 ix2
+  compare (BGlobal did1) (BGlobal did2) = compare did1 did2
+  compare (BLocal _ _) (BGlobal _) = LT
+  compare (BGlobal _) (BLocal _ _) = GT
 
 data ElabContext = ElabContext
-  { unBindings :: Map Name Binding }
+  { unBindings :: Map Name (Set Binding) }
 
 data Predeclaration = PDDecl DeclarationAst | PDConstr ConstructorAst
 
 data ElabState = ElabState
   { unDecls :: Map Id Predeclaration
   , unDeclTypes :: Map Id N.Term
-  , unUVs :: Map Global N.Term
   , unStageUVs :: Map Global (Maybe Stage)
-  , unTypeUVs :: Map Global (Maybe N.Term)
-  , unNextUV :: Global }
+  , unTypeUVs :: Map Global (Maybe N.Term) }
 
-type Elab sig m = (MonadFail m, Has (Reader ElabContext) sig m, Has (State ElabState) sig m, Norm sig m, Query sig m, Has (Throw ()) sig m)
+type Elab sig m =
+  ( MonadFail m
+  , Has (Reader ElabContext) sig m
+  , Has (State ElabState) sig m
+  , Has (Throw ()) sig m
+  , Has NonDet sig m
+  , Norm sig m
+  , Query sig m
+  , Unify sig m )
 
 unify :: Elab sig m => N.Term -> N.Term -> m ()
 unify = undefined
@@ -79,7 +94,7 @@ unify = undefined
 bindLocal :: Elab sig m => Name -> N.Term -> m a -> m a
 bindLocal name ty act =
   local
-    (\ctx -> ctx { unBindings = insert name (BLocal (Index 0) ty) (fmap inc (unBindings ctx))})
+    (\ctx -> ctx { unBindings = insert name (singleton (BLocal (Index 0) ty)) (fmap (Set.map inc) (unBindings ctx)) })
     act
   where
     inc (BLocal ix ty) = BLocal (ix + 1) ty
@@ -105,15 +120,20 @@ addDecls (decl@(DeclAst (Datatype _ _ constrs) _):decls) act = do
     (\ctx -> ctx
       { unBindings =
         union
-          (insert (unDeclName decl) (BGlobal (unId decl)) (unBindings ctx))
-          (fromList (zip (map unConstrName constrs) (map (BGlobal . unCId) constrs))) })
+          (insert (unDeclName decl) (singleton (BGlobal (unId decl))) (unBindings ctx))
+          (fromList (zip (map unConstrName constrs) (map (singleton . BGlobal . unCId) constrs))) })
     (addDecls decls act)
 addDecls (decl:decls) act = do
   state <- get
   put (state { unDecls = insert (unId decl) (PDDecl decl) (unDecls state) })
   local
-    (\ctx -> ctx { unBindings = insert (unDeclName decl) (BGlobal (unId decl)) (unBindings ctx) })
+    (\ctx -> ctx { unBindings = insert (unDeclName decl) (singleton (BGlobal (unId decl))) (unBindings ctx) })
     (addDecls decls act)
+
+lookupBinding :: Elab sig m => Name -> m (Maybe [Binding])
+lookupBinding name = do
+  bindings <- unBindings <$> ask
+  pure (fmap toList (lookup name bindings))
 
 getDecl :: Elab sig m => Id -> m Predeclaration
 getDecl did = do
@@ -122,19 +142,19 @@ getDecl did = do
 
 freshTypeUV :: Elab sig m => m N.Term
 freshTypeUV = do
-  state <- get
-  put (state
-    { unNextUV = unNextUV state + 1
-    , unTypeUVs = insert (unNextUV state) Nothing (unTypeUVs state) })
-  pure (N.UniVar (unNextUV state))
+  state <- get @ElabState
+  UnifyState uv <- get @UnifyState
+  put (state { unTypeUVs = insert uv Nothing (unTypeUVs state) })
+  put (UnifyState (uv + 1))
+  pure (N.UniVar uv)
 
 freshStageUV :: Elab sig m => m Stage
 freshStageUV = do
-  state <- get
-  put (state
-    { unNextUV = unNextUV state + 1
-    , unStageUVs = insert (unNextUV state) Nothing (unStageUVs state) })
-  pure (UniVar (unNextUV state))
+  state <- get @ElabState
+  UnifyState uv <- get @UnifyState
+  put (state { unStageUVs = insert uv Nothing (unStageUVs state) })
+  put (UnifyState (uv + 1))
+  pure (UniVar uv)
 
 report :: Elab sig m => Error -> m ()
 report _ = pure ()
