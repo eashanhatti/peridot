f59ff00241461721f74e49966c6cbbcd44f49744
Eashan Hatti <eashanhatti@gmail.com>
2022-03-05 09:50:50 -0500

    Implement STG syntax

--- src/Syntax/STG.hs
+++ src/Syntax/STG.hs
@@ -1,1 +1,15 @@
 module Syntax.STG where
+
+import Syntax.Extra
+
+data Declaration
+  = Fun [Id] [(RuntimeRep, Id)] RuntimeRep ComplexTerm
+  | Thunk ComplexTerm
+
+data ComplexTerm
+  = Let [(Id, Declaration)] [(Id, SimpleTerm)] SimpleTerm
+  | Val SimpleTerm
+
+data SimpleTerm
+  = Var Id
+  | App SimpleTerm [SimpleTerm]
