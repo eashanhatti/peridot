aa69325ddfd871446a2a24e7587cfd9ba577526a
Eashan Hatti <eashanhatti@gmail.com>
2022-05-16 21:51:30 -0400

    Add skeleton of constraint processing

--- src/Elaboration/Match.hs
+++ src/Elaboration/Match.hs
@@ -1,5 +1,7 @@
 module Elaboration.Match where
 
+import Control.Effect.Reader
+import Control.Effect.Error
 import Data.Sequence
 import Elaboration.Effect
 import Data.Map qualified as Map
@@ -9,22 +11,30 @@ import Syntax.Semantic qualified as N
 import Syntax.Core qualified as C
 import Normalization
 import Extra
+import Control.Monad
+import Prelude hiding(filter, concatMap, unzip)
 
 data ConstraintPattern
-  = CPCon Global Id (Seq ConstraintPattern)
+  = CPCon Id (Seq ConstraintPattern)
   | CPBind Global
 
-data PDClause = PDClse (Seq (ConstraintPattern, PatternAst)) PatternAst TermAst
+type BoundVar = (NameAst, N.Term)
+
+type Constraint = (ConstraintPattern, PatternAst)
+
+data PDClause = PDClse (Seq Constraint) PatternAst TermAst (Seq BoundVar)
 
 check :: Elab sig m => Seq ClauseAst -> N.Term -> m C.Term
 check cls goal = check' goal cls'
   where
     cls' = fmap
-      (\(ClseAst (Clse pat body)) -> PDClse Empty pat body)
+      (\(ClseAst (Clse pat body)) -> PDClse Empty pat body mempty)
       cls
 
 check' :: Elab sig m => N.Term -> Seq PDClause -> m C.Term
-check' (N.MetaFunElims (N.Rigid (N.ObjConstIntro did)) args) _ = undefined
+check' (N.MetaFunElims (N.Rigid (N.ObjConstIntro did)) args) _ = do
+  constrs <- Map.lookup did . unConstrs <$> ask
+  undefined
 check' (N.ObjFunType inTy outTy) cls = do
   let cls' = stripAppPat cls
   case cls' of
@@ -38,8 +48,25 @@ check' (N.Neutral ty _) cls = do
     Just ty -> check' ty cls
     Nothing -> fst <$> errorTerm Todo
 
+processCPs :: Elab sig m => PDClause -> m PDClause
+processCPs (PDClse cs pat body subst) = do
+  (cs', subst') <-
+    unzip <$>
+    (complete <=<
+    concatMapM simplify $
+    cs)
+  pure (PDClse cs' pat body (subst <> subst'))
+  where
+    simplify :: Elab sig m => Constraint -> m (Seq Constraint)
+    simplify (CPCon did cps, PatAst (PCon name ps)) = do
+      undefined
+    simplify c = pure (singleton c)
+
+    complete :: Elab sig m => Seq Constraint -> m (Seq (Constraint, BoundVar))
+    complete = undefined
+
 stripAppPat :: Seq PDClause -> Maybe (Seq (Global -> PDClause))
 stripAppPat pcs = allJustOrNothing (fmap go pcs) where
-  go (PDClse cs (PatAst (PApp (p :<| ps))) body) =
-    Just (\gl -> PDClse ((CPBind gl, p) <| cs) (PatAst (PApp ps)) body)
+  go (PDClse cs (PatAst (PApp (p :<| ps))) body subst) =
+    Just (\gl -> PDClse ((CPBind gl, p) <| cs) (PatAst (PApp ps)) body subst)
   go _ = Nothing
